7f4aacd1f17d8abf9ae652167caad91078738bce
Matt <12matQ@gmail.com>
2018-10-17 23:05:43 +0200

    Update totally_not_a_virus.py

--- totally_not_a_virus.py
+++ totally_not_a_virus.py
@@ -1,5 +1,12 @@
+import msvcrt
+
 def main():
     print("Hello")
+    input("Press any key to start logging.")
+    while True:
+        keystroke = msvcrt.getch()
+        if keystroke != None:
+            print('You pressed a key: ' + str(keystroke))
     input("Press Enter to exit.")
 
 if __name__ == "__main__":
